a6a5ea32b4ead78b773b6b437ed003c6fb5cd766
Syed sadhik <syedsadhik16@gmail.com>
2026-07-07 02:12:59 +0530

    Add landing page route

---
+++ src/app/landing/page.tsx
@@ -0,0 +1,14 @@
+export default function LandingPage() {
+  return (
+    <iframe
+      src="/lifesonce-landing-fixed.html"
+      title="Life's Once Landing Page"
+      style={{
+        width: "100%",
+        height: "100vh",
+        border: "0",
+        display: "block",
+      }}
+    />
+  );
+}
